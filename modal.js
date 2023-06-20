9a86302ee6a2da6f1a727153b0f0726dcb31b9ac
frumilly <ouerfelli.fzahra2@gmail.com>
2023-06-21 00:46:49 +0200

    ne pas utiliser les var fin issue #2

--- modal.js
+++ modal.js
@@ -1,5 +1,8 @@
 const closeBtn = document.querySelector(".close");
 const closeBtn2 = document.querySelector(".fermer");
+// Clonez le formulaire initial
+const originalFormElement = document.getElementById("reserve_f").cloneNode(true);
+
 function editNav() {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
@@ -22,10 +25,10 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 // Sélectionnez le formulaire à réinitialiser
-var form = document.forms.reserve;
+let form = document.forms.reserve;
 // Fonction pour effacer les messages d'erreur
 function clearErrorMessages() {
-  var errorMessages = document.querySelectorAll('.error-message');
+  let errorMessages = document.querySelectorAll('.error-message');
   errorMessages.forEach(function(errorMessage) {
     errorMessage.remove();
   });
@@ -33,6 +36,12 @@ function clearErrorMessages() {
 // Fonction pour réinitialiser le formulaire
 function resetForm() {
   form.reset();
+  let locationError3 = document.getElementById("date_error");
+  locationError3.textContent = "";
+  let locationError1 = document.getElementById("location-error2");
+  locationError1.textContent = "";
+  let locationError2 = document.getElementById("location-error1");
+  locationError2.textContent = "";
   clearErrorMessages();
 }
 
@@ -40,6 +49,17 @@ function closeModal() {
   modalbg.style.display = "none"; 
   resetForm();
 }
+function closeModal2() {
+ 
+   // 
+
+    // Masquer le message de réussite
+    const successMessageElement = document.getElementById("success-message-container1");
+    successMessageElement.style.display = "none";
+    const formElement = document.getElementById("reserve_f");
+    formElement.style.display = "block";
+    modalbg.style.display = "none"; 
+}
 
 closeBtn.addEventListener("click", closeModal);
-closeBtn2.addEventListener("click", closeModal);
+closeBtn2.addEventListener("click", closeModal2);
--- valider_form.js
+++ valider_form.js
@@ -1,18 +1,16 @@
 // Récupérer la référence de l'élément bouton
 const submitButton = document.querySelector('.btn-submit');
-let isValid=true;
+let isValid = true;
+
 // Ajouter un gestionnaire d'événement pour l'événement 'click'
-submitButton.addEventListener('click', function(event) {
+submitButton.addEventListener('click', function (event) {
   // Empêcher le comportement par défaut du bouton (rechargement de la page)
   event.preventDefault();
-  clearErrorMessages(); 
-  var locationError1 = document.getElementById("location-error2");
-locationError1.textContent = "";
-var locationError2 = document.getElementById("location-error1");
-locationError2.textContent = "";
-var locationError3 = document.getElementById("date_error");
-locationError3.textContent = "";
-isValid=true;
+  clearErrorMessages();
+
+
+  
+  isValid = true;
   // Récupérer la référence de l'élément input
   const firstInput = document.getElementById('first');
 
@@ -20,27 +18,27 @@ isValid=true;
   const value = firstInput.value.trim();
 
   // Vérifier si la valeur a un minimum de 2 caractères et n'est pas vide
-  if (value.length <= 2 || value == '') {
+  if (value.length < 2 || value == '') {
 
     console.log('Le champ "first" est invalide.');
     // Ajoutez ici le code à exécuter si le champ est invalide
-   
-    isValid=false;
+
+    isValid = false;
     displayError(firstInput, 'Veuillez entrer 2 caractères ou plus pour le champ du prénom.');
   }
-   // Récupérer la référence de l'élément input du nom
-   const lastInput = document.getElementById('last');
+  // Récupérer la référence de l'élément input du nom
+  const lastInput = document.getElementById('last');
 
-   // Récupérer la valeur saisie dans le champ du nom
-   const lastName = lastInput.value.trim();
- 
-   // Vérifier si le nom a un minimum de 2 caractères et n'est pas vide
-   if (lastName.length <= 2 || lastName == '') {
-     console.log('Le champ "Nom" est invalide.');
-     isValid=false;
-     displayError(lastInput, 'Veuillez entrer 2 caractères ou plus pour le champ du nom.');
-   }
-   // Récupérer la référence de l'élément input de l'adresse électronique
+  // Récupérer la valeur saisie dans le champ du nom
+  const lastName = lastInput.value.trim();
+
+  // Vérifier si le nom a un minimum de 2 caractères et n'est pas vide
+  if (lastName.length < 2 || lastName == '') {
+    console.log('Le champ "Nom" est invalide.');
+    isValid = false;
+    displayError(lastInput, 'Veuillez entrer 2 caractères ou plus pour le champ du nom.');
+  }
+  // Récupérer la référence de l'élément input de l'adresse électronique
   const emailInput = document.getElementById('email');
 
   // Récupérer la valeur saisie dans le champ de l'adresse électronique
@@ -49,44 +47,47 @@ isValid=true;
   // Vérifier si l'adresse électronique est valide
   if (!validateEmail(emailA)) {
     console.log('L\'adresse électronique n\'est pas valide.');
-   isValid=false;
-   displayError(emailInput, 'Email incorrect');
+    isValid = false;
+    displayError(emailInput, 'Email incorrect');
   }
 
   // Récupérer la valeur de la date de naissance à partir de l'élément input
-const birthdateInput = document.getElementById("birthdate");
-const date_verif = birthdateInput.value;
-const birthdateValue = new Date(birthdateInput.value);
+  const birthdateInput = document.getElementById("birthdate");
 
-// Définir les limites d'âge
-const ageLimitMin = new Date();
-ageLimitMin.setFullYear(ageLimitMin.getFullYear() - 65);
+  const date_verif = birthdateInput.value;
+  const birthdateValue = new Date(birthdateInput.value);
 
-const ageLimitMax = new Date();
-ageLimitMax.setFullYear(ageLimitMax.getFullYear() - 18);
-// Vérifier si la date de naissance est valide
-if (date_verif =="") { document.getElementById('date_error').textContent = 'Date de naissance invalide';
-isValid=false;}
+  // Définir les limites d'âge
+  const ageLimitMin = new Date();
+  ageLimitMin.setFullYear(ageLimitMin.getFullYear() - 65);
+
+  const ageLimitMax = new Date();
+  ageLimitMax.setFullYear(ageLimitMax.getFullYear() - 18);
+  // Vérifier si la date de naissance est valide
+  if (date_verif == "") {
+    document.getElementById('date_error').textContent = 'Vous devez entrer votre date de naissance.';
+    isValid = false;
+  }
   // Comparer la date de naissance avec les limites d'âge
   if (birthdateValue < ageLimitMin) {
-    document.getElementById('date_error').textContent = 'Date de naissance invalide';
-    isValid=false;
+    document.getElementById('date_error').textContent = 'Vous êtes encore mineur';
+    isValid = false;
   } else if (birthdateValue > ageLimitMax) {
-    document.getElementById('date_error').textContent = 'Date de naissance invalide';
-    isValid=false;
-  } 
+    document.getElementById('date_error').textContent = 'L\'âge limite est de 65 ans';
+    isValid = false;
+  }
 
- // Récupérer la référence de l'élément input
- const quantityInput = document.getElementById('quantity');
+  // Récupérer la référence de l'élément input
+  const quantityInput = document.getElementById('quantity');
 
- // Récupérer la valeur saisie dans le champ
- const quantity = quantityInput.value.trim();
+  // Récupérer la valeur saisie dans le champ
+  const quantity = quantityInput.value.trim();
 
- // Vérifier si la valeur saisie est numérique
- if ((isNaN(quantity)) || (quantity<0) ){
-   console.log('La valeur saisie n\'est pas numérique.');
-   displayError(quantityInput, 'Veuillez entrer une valeur numérique positive.');
- }
+  // Vérifier si la valeur saisie est numérique
+  if ((isNaN(quantity)) || (quantity < 0)) {
+    console.log('La valeur saisie n\'est pas numérique.');
+    displayError(quantityInput, 'Veuillez entrer une valeur numérique positive.');
+  }
 
   // Récupérer les références des boutons radio avec le même nom de groupe
   const locationInputs = document.getElementsByName('location');
@@ -104,8 +105,8 @@ isValid=false;}
   // Vérifier si au moins un bouton radio est sélectionné
   if (!isLocationSelected) {
     console.log('Aucun bouton radio n\'est sélectionné.');
-    document.getElementById('location-error1').textContent = 'Veuillez sélectionner un tournoi.';
-   isValid=false;
+    document.getElementById('location-error1').textContent = 'Vous devez choisir une option.';
+    isValid = false;
   }
 
   // Récupérer la référence de l'élément de la première case à cocher
@@ -114,20 +115,21 @@ isValid=false;}
   // Vérifier si la première case à cocher est cochée
   if (!checkbox1.checked) {
     console.log('La première case à cocher est obligatoire.');
-    document.getElementById('location-error2').textContent = 'Veuillez accepter les termes et conditions.';
-    isValid=false;
+    document.getElementById('location-error2').textContent = 'Vous devez vérifier que vous acceptez les termes et conditions.';
+    isValid = false;
   }
-if(isValid==true){
-  const formElement = document.getElementById("reserve_f");
-  const successMessageElement = document.getElementById("success-message-container1");
-  
+  if (isValid == true) {
+    const formElement = document.getElementById("reserve_f");
+    const successMessageElement = document.getElementById("success-message-container1");
 
-  // Supprimer le formulaire
-  formElement.remove();
 
-  // Afficher le message de remerciement
-  successMessageElement.style.display = "block";
-}
+    // Supprimer le formulaire
+    //formElement.remove();
+    resetFormulaire();
+formElement.style.display = "none";
+    // Afficher le message de remerciement
+    successMessageElement.style.display = "block";
+  }
 
 });
 function validateEmail(email) {
@@ -135,33 +137,41 @@ function validateEmail(email) {
   return emailRegex.test(email);
 }
 function clearErrorMessages() {
-  var errorMessages = document.querySelectorAll('.error-message');
-  errorMessages.forEach(function(errorMessage) {
+  let errorMessages = document.querySelectorAll('.error-message');
+  errorMessages.forEach(function (errorMessage) {
     errorMessage.remove();
   });
 }
 
 // Fonction pour afficher un message d'erreur sous un champ de saisie
 function displayError(inputElement, errorMessage) {
-    // Supprimer les messages d'erreur existants
-    const existingErrorMessage = inputElement.parentElement.querySelector('.error-message');
-    if (existingErrorMessage) {
-      existingErrorMessage.remove();
-    }
-  
-    // Créer un nouvel élément de paragraphe pour le message d'erreur
-    const errorElement = document.createElement('p');
-    errorElement.classList.add('error-message','small-error-message');
-    errorElement.textContent = errorMessage;
-  
-    // Insérer le message d'erreur sous le champ de saisie
-    inputElement.parentElement.appendChild(errorElement);
+  // Supprimer les messages d'erreur existants
+  const existingErrorMessage = inputElement.parentElement.querySelector('.error-message');
+  if (existingErrorMessage) {
+    existingErrorMessage.remove();
   }
- 
-  
-  
-  
-  
-  
-  
-  
+
+  // Créer un nouvel élément de paragraphe pour le message d'erreur
+  const errorElement = document.createElement('p');
+  errorElement.classList.add('error-message', 'small-error-message');
+  errorElement.textContent = errorMessage;
+
+  // Insérer le message d'erreur sous le champ de saisie
+  inputElement.parentElement.appendChild(errorElement);
+}
+
+let  form2 = document.forms.reserve;
+function resetFormulaire() {
+  form2.reset();
+  let locationError3 = document.getElementById("date_error");
+  locationError3.textContent = "";
+  let locationError1 = document.getElementById("location-error2");
+  locationError1.textContent = "";
+  let locationError2 = document.getElementById("location-error1");
+  locationError2.textContent = "";
+  clearErrorMessages();
+}
+
+
+
+
